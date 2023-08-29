59727242330a98ae2d672f32ce279446193a09ca
Biancaa.R <87770461+Biancaa-R@users.noreply.github.com>
2023-08-29 14:52:52 +0530

    Update main1.py

--- incu_parameters/main1.py
+++ incu_parameters/main1.py
@@ -285,7 +285,7 @@ class TeleScreen(QDialog):
                             #humidity = round(humidity, 2)
                             #temperature = round(temperature, 2)
 
-        bot = telepot.Bot('6355723537:AAHCm412-9KGQFhM6xoO0C1D-DXsLMLvdYc')
+        bot = telepot.Bot('api key of bot')
 
         def handle(msg):
             content_type, chat_type, chat_id = telepot.glance(msg)
