0c26b9dc8f0becd5a3028a1ab58b7d538f023861
Biancaa.R <87770461+Biancaa-R@users.noreply.github.com>
2023-08-29 09:49:00 +0100

    Add files via upload

---
+++ hum_res_proj/hum_res_proj.ino
@@ -0,0 +1,54 @@
+#include "DHT.h"
+
+#define DHTPIN 2     // Digital pin connected to the DHT sensor
+
+#define DHTTYPE DHT11   // DHT 11
+
+const int sensorPin = A0;  // Analog input pin that senses Vout
+int sensorValue = 0;       // sensorPin default value
+float Vin = 5;             // Input voltage
+float Vout = 0;            // Vout default value
+float Rref = 1000;          // Reference resistor's value in ohms (you can give this value in kiloohms or megaohms - the resistance of the tested resistor will be given in the same units)
+float R = 0;               // Tested resistors default value
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println(F("DHTxx test!"));
+
+  dht.begin();
+}
+
+void loop() {
+  // Wait a few seconds between measurements.
+  delay(2000);
+
+  float h = dht.readHumidity();
+  // Read temperature as Celsius (the default)
+  float t = dht.readTemperature();
+  // Read temperature as Fahrenheit (isFahrenheit = true)
+  float f = dht.readTemperature(true);
+
+  sensorValue = analogRead(sensorPin);  // Read Vout on analog input pin A0 (Arduino can sense from 0-1023, 1023 is 5V)
+  Vout = (Vin * sensorValue) / 1023;    // Convert Vout to volts
+
+  R= ((( Vin* Rref)/Vout)-Rref);
+  
+
+  // Check if any reads failed and exit early (to try again).
+  if (isnan(h) || isnan(t) || isnan(f)) {
+    Serial.println(F("Failed to read from DHT sensor!"));
+    return;
+  }
+
+
+
+  Serial.print(F("Humidity: "));
+  Serial.print(h);
+  Serial.print(F("%  Temperature: "));
+  Serial.print(t);
+  Serial.print(F("°C "));
+  Serial.print("R: ");                  
+  Serial.println(R);                    // Give calculated resistance in Serial Monitor
+  delay(1000);
+}
